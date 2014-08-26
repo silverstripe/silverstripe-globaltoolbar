20afb76dcb273c56a457a3a62c8effb50e84cffd
Stephen Shkardoon <stephen@silverstripe.com>
2014-08-26 18:06:17 +1200

    Fix standards

--- code/GlobalNavIteratorProperties.php
+++ code/GlobalNavIteratorProperties.php
@@ -1,34 +1,29 @@
 <?php
 
+class GlobalNavIteratorProperties implements TemplateIteratorProvider {
 
-class GlobalNavIteratorProperties implements TemplateIteratorProvider
-{
+	public static function get_template_iterator_variables() {
+		return array(
+			'GlobalNav',
+		);
+	}
 
-    public static function get_template_iterator_variables() {
-        return array(
-            'GlobalNav'
-        );
-    }
+	public function iteratorProperties($pos, $totalItems) {
+		$host = GlobalNavSiteTreeExtension::get_toolbar_hostname();
+		$path = Config::inst()->get('GlobalNav','snippet_path');
+		$html = @file_get_contents(Controller::join_links($host,$path));
+		if (empty($html)) {
+			$this->globalNav = '';
+		}
+		$this->globalNav = $html;
+	}
 
-    public function iteratorProperties($pos, $totalItems) {
-        $host = GlobalNavSiteTreeExtension::get_toolbar_hostname();
-        $path = Config::inst()->get('GlobalNav','snippet_path');
-        $html = @file_get_contents(Controller::join_links($host,$path));
-        if (empty($html)) {
-            $html = ''; // TODO: decide what the default should be
-        }
-        $this->globalNav = $html;
-    }
+	public function GlobalNav() {
+		Requirements::css(Controller::join_links(GlobalNavSiteTreeExtension::get_toolbar_hostname(), Config::inst()->get('GlobalNav','css_path')));
 
+		$html = DBField::create_field('HTMLText',$this->globalNav);
+		$html->setOptions(array('shortcodes' => false));
 
-
-    public function GlobalNav() {
-        Requirements::css(Controller::join_links(GlobalNavSiteTreeExtension::get_toolbar_hostname(), Config::inst()->get('GlobalNav','css_path')));
-
-        $html = DBField::create_field('HTMLText',$this->globalNav);
-        $html->setOptions(array('shortcodes' => false));
-
-        return $html;
-    }
-
+		return $html;
+	}
 }
--- code/GlobalNavSiteTreeExtension.php
+++ code/GlobalNavSiteTreeExtension.php
@@ -1,78 +1,71 @@
 <?php
 
-class GlobalNavSiteTreeExtension extends DataExtension
-{
+class GlobalNavSiteTreeExtension extends DataExtension {
 
-    private static $db = array (
-        'ShowInGlobalNav' => 'Boolean',
-        'ShowChildrenInGlobalNav' => 'Boolean'
-    );
-
-
-    public static function get_toolbar_hostname() {
-        return  Config::inst()->get('GlobalNav','use_localhost') ? 
-                Director::absoluteBaseURL() :
-                Config::inst()->get('GlobalNav','hostname');        
-    }
-
-    public static function create_nav() {        
-        $html = ViewableData::create()->customise(array(
-            'ToolbarHostname' => self::get_toolbar_hostname(),
-            'Pages' => SiteTree::get()->filter(array(
-                'ParentID' => 0,
-                'ShowInGlobalNav' => true
-            )),
-            'GoogleCustomSearchId' => Config::inst()->get('GlobalNav', 'google_search_id')
-        ))->renderWith('GlobalNavbar');
-
-        $path = Config::inst()->get('GlobalNav','snippet_path');
-        
-        file_put_contents(BASE_PATH.$path, $html);
-    }    
+	private static $db = array(
+		'ShowInGlobalNav' => 'Boolean',
+		'ShowChildrenInGlobalNav' => 'Boolean',
+	);
 
 
-    public function updateSettingsFields(FieldList $fields) {
-        $p = $this->owner->Parent();
-        if(!$p->exists()) {
-            $fields->addFieldToTab("Root.Settings", CheckboxField::create('ShowInGlobalNav','Show in global nav'));
-            $fields->addFieldToTab("Root.Settings", CheckboxField::create('ShowChildrenInGlobalNav','Include child pages')
-                                                        ->displayIf('ShowInGlobalNav')->isChecked()
-                                                        ->end()
-            );
-        }
+	public static function get_toolbar_hostname() {
+		return Config::inst()->get('GlobalNav','use_localhost') ? Director::absoluteBaseURL() : Config::inst()->get('GlobalNav','hostname');
+	}
 
-        else if($p->ShouldShowChildren()) {
-            $fields->addFieldToTab("Root.Settings", CheckboxField::create('ShowInGlobalNav','Show in global nav'));        
-        }
-    }
+	public static function create_nav() {
+		$html = ViewableData::create()->customise(array(
+				'ToolbarHostname' => self::get_toolbar_hostname(),
+				'Pages' => SiteTree::get()->filter(array(
+				'ParentID' => 0,
+				'ShowInGlobalNav' => true
+			)),
+			'GoogleCustomSearchId' => Config::inst()->get('GlobalNav', 'google_search_id')
+		))->renderWith('GlobalNavbar');
 
+		$path = Config::inst()->get('GlobalNav', 'snippet_path');
 
-    public function ShouldShowChildren() {
-        return $this->owner->ShowInGlobalNav && $this->owner->ShowChildrenInGlobalNav;
-    }
-
-
-    public function GlobalNavLink() {
-        if($this->owner instanceof RedirectorPage && $this->owner->ExternalURL) {
-            return $this->owner->ExternalURL;
-        }
-
-        return Controller::join_links(Director::absoluteBaseURL(), $this->owner->Link());
-    }
+		file_put_contents(BASE_PATH . $path, $html);
+	}    
 
 
-    public function GlobalNavChildren() {
-        return $this->owner->Children()
-                    ->filter(array(
-                        'ShowInGlobalNav' => true
-                    ));
-    }
+	public function updateSettingsFields(FieldList $fields) {
+		$p = $this->owner->Parent();
+		if (!$p->exists()) {
+			$fields->addFieldToTab("Root.Settings", CheckboxField::create('ShowInGlobalNav','Show in global nav'));
+			$fields->addFieldToTab("Root.Settings", CheckboxField::create('ShowChildrenInGlobalNav','Include child pages')
+				->displayIf('ShowInGlobalNav')->isChecked()
+				->end()
+			);
+		} else if ($p->ShouldShowChildren()) {
+			$fields->addFieldToTab("Root.Settings", CheckboxField::create('ShowInGlobalNav','Show in global nav'));
+		}
+	}
 
 
-    public function onAfterWrite() {
-        if($this->owner->ParentID == 0) {
-            self::create_nav();
-        }
-    }
+	public function ShouldShowChildren() {
+		return $this->owner->ShowInGlobalNav && $this->owner->ShowChildrenInGlobalNav;
+	}
+
+
+	public function GlobalNavLink() {
+		if($this->owner instanceof RedirectorPage && $this->owner->ExternalURL) {
+			return $this->owner->ExternalURL;
+		}
+		return Controller::join_links(Director::absoluteBaseURL(), $this->owner->Link());
+	}
+
+
+	public function GlobalNavChildren() {
+		return $this->owner->Children()->filter(array(
+			'ShowInGlobalNav' => true,
+		));
+	}
+
+
+	public function onAfterWrite() {
+		if ($this->owner->ParentID == 0) {
+			self::create_nav();
+		}
+	}
 
 }
