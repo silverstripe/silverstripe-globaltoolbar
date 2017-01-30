60d011a85b53c69b395beb09412af3f6d3884f8c
Robbie Averill <robbie@silverstripe.com>
2017-01-31 11:32:57 +1300

    FIX Replace use of ThemeDir with ThemeResourceLoader

--- src/Controller.php
+++ src/Controller.php
@@ -4,6 +4,8 @@ namespace SilverStripe\Toolbar;
 
 use SilverStripe\Control\Controller as BaseController;
 use SilverStripe\View\Requirements;
+use SilverStripe\View\SSViewer;
+use SilverStripe\View\ThemeResourceLoader;
 
 class Controller extends BaseController
 {
@@ -20,5 +22,16 @@ class Controller extends BaseController
     public function profile($request)
     {
         return $this->renderWith('GlobalToolbarProfile');
+    }
+
+    /**
+     * Find a file by theme
+     *
+     * @param  string $filename A file path relative to the root folder of a theme
+     * @return string|null
+     */
+    public function getThemedFile($filename)
+    {
+        return ThemeResourceLoader::instance()->findThemedResource($filename, SSViewer::get_themes());
     }
 }
